c623415af27902beec40180b91611b0a954d53d2
Lemuns <toppettichristian@gmail.com>
2023-11-05 00:40:52 -0300

    Preparando segunda pre-entrega

---
+++ segunda pre-entrega/src/server.js
@@ -0,0 +1,59 @@
+import http from 'http'
+import { Server } from 'socket.io'
+import app from './app.js'
+import ProductManager from './dao/ProductManager.js'
+import ChatManager from './dao/ChatManager.js'
+import { init } from './db/mongodb.js'
+
+await init()
+
+const server = http.createServer(app)
+const socketServer = new Server(server)
+const PORT = 8080
+
+const getProducts = async () => {
+    return (await ProductManager.getProducts()).payload
+}
+
+socketServer.on('connection', async (socket) => {
+    // Productos
+    socket.on('send-products', async () => {
+        socketServer.emit('update-products', await getProducts())
+    })
+
+    socket.on('new-product', async ({name, price, stock, description, code, category}) => {
+        const testThumbnail = './thumbnail1.webp'
+        const newProduct = {
+            title:name, 
+            description, 
+            code, 
+            price, 
+            status:true, 
+            stock, 
+            thumbnail:testThumbnail,
+            category: category
+        }
+        await ProductManager.addProduct(newProduct)
+    
+        socketServer.emit('update-products', await getProducts())
+    })
+    
+    socket.on('delete-product', async (index) => {
+        await ProductManager.deleteProduct(index)
+        socketServer.emit('update-products', await getProducts())
+    })
+
+    // Chat
+    socket.on('send-chat', async () => {
+        socket.emit('update-chat', await ChatManager.getMessages())
+    })
+
+    socket.on('new-message', async ({user, message}) => {
+        await ChatManager.addMessage({user, message})
+        socketServer.emit('update-message', {user, message})
+    })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running in http://localhost:${PORT}/`)
+})
