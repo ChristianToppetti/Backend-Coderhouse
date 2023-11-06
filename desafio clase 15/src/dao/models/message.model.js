01aaf8ba6b6fa36b549198c7e0367573faaaee02
Lemuns <toppettichristian@gmail.com>
2023-11-05 23:43:53 -0300

    Depuracion

--- desafio clase 15/src/dao/models/message.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const messageSchema = new mongoose.Schema({
-  user: { type: String }, 
-  message: { type: String }
-})
-
-export default mongoose.model('message', messageSchema);
